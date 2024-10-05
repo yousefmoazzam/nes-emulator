e79c096d200c17f5285346a5e6d75b65989ee121
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-10-05 17:52:09 +0100

    Set zero flag in `BIT` instruction implementation

--- src/cpu.rs
+++ src/cpu.rs
@@ -352,6 +352,7 @@ impl<'a> CPU<'a> {
     fn bit(&mut self, mode: &AddressingMode) {
         let addr = self.get_operand_address(mode);
         let value = self.mem_read(addr);
+        let res = self.register_a & value;
 
         if value & 0b0100_0000 == 0b0100_0000 {
             self.status |= 0b0100_0000;
@@ -363,6 +364,10 @@ impl<'a> CPU<'a> {
             self.status |= 0b1000_0000;
         } else {
             self.status &= 0b0111_1111;
+        }
+
+        if res == 0 {
+            self.status |= 0b0000_0010;
         }
     }
 }
@@ -992,5 +997,33 @@ mod tests {
         cpu.load_and_run(program);
         let is_negative_flag_set = cpu.status & 0b1000_0000 == 0b1000_0000;
         assert_eq!(is_negative_flag_set, false);
+    }
+
+    #[test]
+    fn bit_zero_page_addressing_mode_sets_zero_flag() {
+        let mut ram = [0x00; 0xFFFF];
+        let zero_page_addr = 0x15;
+        let memory_value = 0b0010_0100;
+        let register_a_value = 0b1101_1011;
+        ram[zero_page_addr as usize] = memory_value;
+
+        let mut cpu = CPU::new(&mut ram);
+        let lda_immediate_addr_mode_opcode = 0xA9;
+        let bit_zero_page_addr_mode_opcode = 0x24;
+
+        // Program does the following:
+        // - load value into register A
+        // - perform BIT instruction with value in address `zero_page_addr`
+        // - break
+        let program = vec![
+            lda_immediate_addr_mode_opcode,
+            register_a_value,
+            bit_zero_page_addr_mode_opcode,
+            zero_page_addr,
+            0x00,
+        ];
+        cpu.load_and_run(program);
+        let is_zero_flag_set = cpu.status & 0b000_0010 == 0b0000_0010;
+        assert_eq!(is_zero_flag_set, true);
     }
 }
