162b69e982739422973df84111b94357cd3e383c
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-10-06 22:59:26 +0100

    Implement `BNE` instruction

--- src/cpu.rs
+++ src/cpu.rs
@@ -168,6 +168,7 @@ impl<'a> CPU<'a> {
                 0xF0 => self.beq(),
                 0x30 => self.bmi(),
                 0x10 => self.bpl(),
+                0xD0 => self.bne(),
                 0x38 => self.sec(),
                 0x18 => self.clc(),
                 _ => todo!(),
@@ -419,6 +420,15 @@ impl<'a> CPU<'a> {
     /// `BPL` instruction
     fn bpl(&mut self) {
         if self.status & 0b1000_0000 == 0 {
+            let offset = self.mem_read(self.program_counter) as i8;
+            let offsetted_program_counter = self.program_counter as i32 + offset as i32;
+            self.program_counter = offsetted_program_counter as u16;
+        }
+    }
+
+    /// `BNE` instruction
+    fn bne(&mut self) {
+        if self.status & 0b0000_0010 == 0 {
             let offset = self.mem_read(self.program_counter) as i8;
             let offsetted_program_counter = self.program_counter as i32 + offset as i32;
             self.program_counter = offsetted_program_counter as u16;
@@ -1277,6 +1287,28 @@ mod tests {
             + no_of_instructions_before_offset_is_read
             + offset as i32
             + increment_after_reading_bpl_instruction;
+        cpu.load_and_run(program);
+        assert_eq!(expected_program_counter_value as u16, cpu.program_counter);
+    }
+
+    #[test]
+    fn bne_correctly_offsets_program_counter() {
+        let mut ram = [0x00; 0xFFFF];
+        let program_counter_start: u16 = 0x8000;
+        let offset = -6i8;
+
+        let mut cpu = CPU::new(&mut ram);
+        let bne_opcode = 0xD0;
+
+        // Program does the following:
+        // - execute BNE instruction (which does non-trivial action if zero flag is clear)
+        let program = vec![bne_opcode, offset as u8];
+        let no_of_instructions_before_offset_is_read = 1;
+        let increment_after_reading_bne_instruction = 1;
+        let expected_program_counter_value = program_counter_start as i32
+            + no_of_instructions_before_offset_is_read
+            + offset as i32
+            + increment_after_reading_bne_instruction;
         cpu.load_and_run(program);
         assert_eq!(expected_program_counter_value as u16, cpu.program_counter);
     }
