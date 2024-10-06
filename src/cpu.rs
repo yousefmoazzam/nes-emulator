9c88cb18d2af15b79fec814e3538fa7a7bed3e29
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-10-06 22:01:45 +0100

    Implement `BEQ` instruction

--- src/cpu.rs
+++ src/cpu.rs
@@ -163,6 +163,7 @@ impl<'a> CPU<'a> {
                     self.bit(&AddressingMode::Absolute);
                     self.program_counter += 2;
                 }
+                0xF0 => self.beq(),
                 _ => todo!(),
             }
         }
@@ -370,6 +371,15 @@ impl<'a> CPU<'a> {
             self.status |= 0b0000_0010;
         } else {
             self.status &= 0b1111_1101;
+        }
+    }
+
+    /// `BEQ` instruction
+    fn beq(&mut self) {
+        if self.status & 0b0000_0010 == 0b0000_0010 {
+            let offset = self.mem_read(self.program_counter) as i8;
+            let offsetted_program_counter = self.program_counter as i32 + offset as i32;
+            self.program_counter = offsetted_program_counter as u16;
         }
     }
 }
@@ -1063,5 +1073,34 @@ mod tests {
         cpu.load_and_run(program);
         let is_zero_flag_set = cpu.status & 0b000_0010 == 0b0000_0010;
         assert_eq!(is_zero_flag_set, false);
+    }
+
+    #[test]
+    fn beq_correctly_offsets_program_counter() {
+        let mut ram = [0x00; 0xFFFF];
+        let program_counter_start: u16 = 0x8000;
+        let offset = -6i8;
+
+        let mut cpu = CPU::new(&mut ram);
+        let lda_immediate_addr_mode_opcode = 0xA9;
+        let beq_opcode = 0xF0;
+
+        // Program does the following:
+        // - load zero value into register A (so then the zero flag is set)
+        // - perform BEQ instruction (which does non-trivial action if the zero flag is set)
+        let program = vec![
+            lda_immediate_addr_mode_opcode,
+            0x00,
+            beq_opcode,
+            offset as u8,
+        ];
+        let no_of_instructions_before_offset_is_read = 3;
+        let increment_after_reading_beq_instruction = 1;
+        let expected_program_counter_value = program_counter_start as i32
+            + no_of_instructions_before_offset_is_read
+            + offset as i32
+            + increment_after_reading_beq_instruction;
+        cpu.load_and_run(program);
+        assert_eq!(expected_program_counter_value as u16, cpu.program_counter);
     }
 }
