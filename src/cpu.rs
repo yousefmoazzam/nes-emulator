cab4693a0aeb0d7305ba0937cd8e6b92501a6db3
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-10-06 22:36:57 +0100

    Implement `BCC` instruction

--- src/cpu.rs
+++ src/cpu.rs
@@ -163,6 +163,7 @@ impl<'a> CPU<'a> {
                     self.bit(&AddressingMode::Absolute);
                     self.program_counter += 2;
                 }
+                0x90 => self.bcc(),
                 0xB0 => self.bcs(),
                 0xF0 => self.beq(),
                 0x38 => self.sec(),
@@ -374,6 +375,15 @@ impl<'a> CPU<'a> {
             self.status |= 0b0000_0010;
         } else {
             self.status &= 0b1111_1101;
+        }
+    }
+
+    /// `BCC` instruction
+    fn bcc(&mut self) {
+        if self.status & 0b0000_0001 == 0 {
+            let offset = self.mem_read(self.program_counter) as i8;
+            let offsetted_program_counter = self.program_counter as i32 + offset as i32;
+            self.program_counter = offsetted_program_counter as u16;
         }
     }
 
@@ -1174,6 +1184,27 @@ mod tests {
             + no_of_instructions_before_offset_is_read
             + offset as i32
             + increment_after_reading_bcs_instruction;
+        cpu.load_and_run(program);
+        assert_eq!(expected_program_counter_value as u16, cpu.program_counter);
+    }
+
+    #[test]
+    fn bcc_correctly_offsets_program_counter() {
+        let mut ram = [0x00; 0xFFFF];
+        let program_counter_start: u16 = 0x8000;
+        let offset = -6i8;
+        let mut cpu = CPU::new(&mut ram);
+        let bcc_opcode = 0x90;
+
+        // Program does the following:
+        // - execute BCC instruction (which does non-trivial action if carry flag is clear)
+        let program = vec![bcc_opcode, offset as u8];
+        let no_of_instructions_before_offset_is_read = 1;
+        let increment_after_reading_bcc_instruction = 1;
+        let expected_program_counter_value = program_counter_start as i32
+            + no_of_instructions_before_offset_is_read
+            + offset as i32
+            + increment_after_reading_bcc_instruction;
         cpu.load_and_run(program);
         assert_eq!(expected_program_counter_value as u16, cpu.program_counter);
     }
