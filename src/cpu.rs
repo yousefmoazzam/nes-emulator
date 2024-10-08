2ab24823707596ef80429ad1dd92f9c1a186e934
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-10-09 00:13:41 +0100

    Clear carry flag in `LSR` instruction implementation

--- src/cpu.rs
+++ src/cpu.rs
@@ -484,6 +484,8 @@ impl<'a> CPU<'a> {
         let is_bit_zero_set = value & 0b0000_0001 == 0b0000_0001;
         if is_bit_zero_set {
             self.status |= 0b0000_0001;
+        } else {
+            self.status &= 0b1111_1110;
         }
 
         let shifted_value = value >> 1;
@@ -1476,5 +1478,30 @@ mod tests {
         cpu.load_and_run(program);
         let is_carry_flag_set = cpu.status & 0b000_0001 == 0b0000_0001;
         assert_eq!(is_carry_flag_set, true);
+    }
+
+    #[test]
+    fn lsr_zero_page_addressing_mode_clears_carry_flag() {
+        let mut ram = [0x00; 0xFFFF];
+        let zero_page_addr = 0x15;
+        let memory_value = 0b0000_0010;
+        ram[zero_page_addr as usize] = memory_value;
+        let mut cpu = CPU::new(&mut ram);
+        let sec_opcode = 0x38;
+        let lsr_zero_page_addr_mode_opcode = 0x46;
+
+        // Program does the following:
+        // - set carry flag
+        // - execute LSR instruction on value in zero page addr (should clear carry flag)
+        // - break
+        let program = vec![
+            sec_opcode,
+            lsr_zero_page_addr_mode_opcode,
+            zero_page_addr,
+            0x00,
+        ];
+        cpu.load_and_run(program);
+        let is_carry_flag_set = cpu.status & 0b0000_0001 == 0b0000_0001;
+        assert_eq!(is_carry_flag_set, false);
     }
 }
