df1fc159ed8a4f058b8a1a2f3d7aff07d4f41b10
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-10-08 23:30:43 +0100

    Set carry flag in `ASL` instruction implementation

--- src/cpu.rs
+++ src/cpu.rs
@@ -453,8 +453,13 @@ impl<'a> CPU<'a> {
     fn asl(&mut self, mode: &AddressingMode) {
         let addr = self.get_operand_address(mode);
         let value = self.mem_read(addr);
-        let shifted_value = value << 1;
 
+        let is_bit_seven_set = value & 0b1000_0000 == 0b1000_0000;
+        if is_bit_seven_set {
+            self.status |= 0b0000_0001;
+        }
+
+        let shifted_value = value << 1;
         if shifted_value == 0 {
             self.status |= 0b0000_0010;
         }
@@ -1344,5 +1349,26 @@ mod tests {
         cpu.load_and_run(program);
         let is_zero_flag_set = cpu.status & 0b000_0010 == 0b0000_0010;
         assert_eq!(is_zero_flag_set, true);
+    }
+
+    #[test]
+    // NOTE: Practically identical to zero flag test, just checking a different bit in the status
+    // register, so maybe some kind of merging of the tests is possible to remove duplication;
+    // parametrisation maybe?
+    fn asl_zero_page_addressing_mode_sets_carry_flag() {
+        let mut ram = [0x00; 0xFFFF];
+        let zero_page_addr = 0x15;
+        let memory_value = 0b1000_0000; // arithmetic left shift produces zero value
+        ram[zero_page_addr as usize] = memory_value;
+        let mut cpu = CPU::new(&mut ram);
+        let asl_zero_page_addr_mode_opcode = 0x06;
+
+        // Program does the following:
+        // - execute ASL instruction on value in zero page addr
+        // - break
+        let program = vec![asl_zero_page_addr_mode_opcode, zero_page_addr, 0x00];
+        cpu.load_and_run(program);
+        let is_carry_flag_set = cpu.status & 0b000_0001 == 0b0000_001;
+        assert_eq!(is_carry_flag_set, true);
     }
 }
