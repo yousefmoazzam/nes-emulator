9aa701cc65a29ae3caa8c6fff299fff3f4b79218
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-10-29 23:36:17 +0000

    Add single address read method to bus struct

--- src/bus.rs
+++ src/bus.rs
@@ -1,3 +1,29 @@
-pub struct Bus {
-    ram: [u8; 2048],
+pub struct Bus<'a> {
+    ram: &'a [u8],
+}
+
+impl<'a> Bus<'a> {
+    pub fn new(ram: &'a [u8]) -> Bus {
+        Bus { ram }
+    }
+
+    pub fn mem_read(&self, addr: u16) -> u8 {
+        self.ram[addr as usize]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn mem_read_returns_correct_value() {
+        let mut ram = [0x00; 2048];
+        let value = 0x15;
+        let addr = 0x0F;
+        ram[addr as usize] = value;
+        let bus = Bus::new(&ram);
+        let read_value = bus.mem_read(addr);
+        assert_eq!(value, read_value);
+    }
 }
