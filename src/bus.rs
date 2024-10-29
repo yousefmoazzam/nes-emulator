63d53c75a7678fb63ffcd3e1bd8a8667c12bb906
Yousef Moazzam <yousefmoazzam@hotmail.co.uk>
2024-10-29 23:44:05 +0000

    Add two-contiguous-addresses read method to bus struct

--- src/bus.rs
+++ src/bus.rs
@@ -10,6 +10,14 @@ impl<'a> Bus<'a> {
     pub fn mem_read(&self, addr: u16) -> u8 {
         self.ram[addr as usize]
     }
+
+    /// Read `u16` value stored in little-endian format, from two contiguous memory addresses each
+    /// storing a single `u8` value
+    pub fn mem_read_u16(&self, pos: u16) -> u16 {
+        let lo = self.mem_read(pos);
+        let hi = self.mem_read(pos + 1);
+        u16::from_le_bytes([lo, hi])
+    }
 }
 
 #[cfg(test)]
@@ -25,5 +33,19 @@ mod tests {
         let bus = Bus::new(&ram);
         let read_value = bus.mem_read(addr);
         assert_eq!(value, read_value);
+    }
+
+    #[test]
+    fn mem_read_u16_returns_correct_value() {
+        let mut ram = [0x00; 2048];
+        let lo = 0x05;
+        let hi = 0xFA;
+        let expected_value = u16::from_le_bytes([lo, hi]);
+        let addr = 0x24;
+        ram[addr as usize] = lo;
+        ram[(addr + 1) as usize] = hi;
+        let bus = Bus::new(&ram);
+        let read_value = bus.mem_read_u16(addr);
+        assert_eq!(expected_value, read_value);
     }
 }
